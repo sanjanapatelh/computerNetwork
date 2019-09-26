01df8b787a1f2d0f80d56b14ab289ee157f3e7d2
sanjanapatelh <48161994+sanjanapatelh@users.noreply.github.com>
2019-09-26 12:17:32 +0530

    Add files via upload

---
+++ UDP/server.py
@@ -0,0 +1,15 @@
+from socket import *
+serverName='127.0.0.1'
+serverPort=12000
+serverSocket=socket(AF_INET,SOCK_DGRAM)
+serverSocket.bind((serverName,serverPort))
+print("the server is ready to recieve")
+while 1:
+    sentence,clientAddress=serverSocket.recvfrom(2048)
+    file=open(sentence,"r")
+    l=file.read(2048)
+    serverSocket.sendto(bytes(l,"utf-8"),clientAddress)
+    print("sent to client")
+file.close()
+    
+    
